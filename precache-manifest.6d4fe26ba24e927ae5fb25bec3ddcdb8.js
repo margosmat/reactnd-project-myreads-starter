cc1f8ebe4b702a915045b39b7635abd16926cd09
Mateusz Margos <mateuszmargos@gmail.com>
2019-01-29 21:07:15 +0100

    Updates

---
+++ precache-manifest.6d4fe26ba24e927ae5fb25bec3ddcdb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/reactnd-project-myreads-starter/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "83f7d75ca12013b90cc6",
+    "url": "/reactnd-project-myreads-starter/static/js/runtime~main.83f7d75c.js"
+  },
+  {
+    "revision": "894c3b828237bee08dbf",
+    "url": "/reactnd-project-myreads-starter/static/js/main.894c3b82.chunk.js"
+  },
+  {
+    "revision": "ae6eb9f02219b7ad7b62",
+    "url": "/reactnd-project-myreads-starter/static/js/1.ae6eb9f0.chunk.js"
+  },
+  {
+    "revision": "894c3b828237bee08dbf",
+    "url": "/reactnd-project-myreads-starter/static/css/main.71842521.chunk.css"
+  },
+  {
+    "revision": "fc4f27e0f0621a0bbb8b6a9466ca5479",
+    "url": "/reactnd-project-myreads-starter/index.html"
+  }
+];
